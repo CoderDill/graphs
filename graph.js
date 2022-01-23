844dc4e488d85cb899ac2321837d9f78720c40c1
CoderDill <mddillon47@yahoo.com>
2022-01-22 19:08:45 -0800

    add/delete done

--- graph.js
+++ graph.js
@@ -11,25 +11,45 @@ class Graph {
   }
 
   // this function accepts a Node instance and adds it to the nodes property on the graph
-  addVertex(vertex) {}
+  addVertex(vertex) {
+    this.nodes.add(vertex);
+  }
 
   // this function accepts an array of Node instances and adds them to the nodes property on the graph
-  addVertices(vertexArray) {}
+  addVertices(vertexArray) {
+    for (let vertex of vertexArray) {
+      this.nodes.add(vertex);
+    }
+  }
 
   // this function accepts two vertices and updates their adjacent values to include the other vertex
-  addEdge(v1, v2) {}
+  addEdge(v1, v2) {
+    
+  }
 
   // this function accepts two vertices and updates their adjacent values to remove the other vertex
   removeEdge(v1, v2) {}
 
   // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
-  removeVertex(vertex) {}
+  removeVertex(vertex) {
+    if (this.nodes.has(vertex)) {
+      this.nodes.delete(vertex)
+    }
+  }
 
   // this function returns an array of Node values using DFS
   depthFirstSearch(start) {}
 
   // this function returns an array of Node values using BFS
-  breadthFirstSearch(start) {}
+  breadthFirstSearch(start) {
+    if (curr === start) return start;
+
+    let toVisit = [start];
+
+    while (toVisit.length) {
+      let curr = toVisit.shift();
+    }
+  }
 }
 
-module.exports = {Graph, Node}
+module.exports = {Node, Graph}
