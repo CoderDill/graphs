c063dc98c7b8987c153a6c7b6a3930d947b4619b
CoderDill <mddillon47@yahoo.com>
2022-01-22 23:50:43 -0800

    dfs and bfs

--- graph.js
+++ graph.js
@@ -24,32 +24,68 @@ class Graph {
 
   // this function accepts two vertices and updates their adjacent values to include the other vertex
   addEdge(v1, v2) {
-    
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
   }
 
   // this function accepts two vertices and updates their adjacent values to remove the other vertex
-  removeEdge(v1, v2) {}
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  }
 
   // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
   removeVertex(vertex) {
     if (this.nodes.has(vertex)) {
-      this.nodes.delete(vertex)
+      this.nodes.delete(vertex);
     }
   }
 
   // this function returns an array of Node values using DFS
-  depthFirstSearch(start) {}
+  depthFirstSearch(start) {
+    const result = [];
+    const seen = new Set();
+
+    function traverse(vertex) {
+      if (!vertex) {
+        return null;
+      }
+      result.push(vertex.value);
+      seen.add(vertex);
+
+      vertex.adjacent.forEach((neighbor) => {
+        if (!seen.has(neighbor)) {
+          return traverse(neighbor);
+        }
+      });
+    }
+    traverse(start);
+
+    return result;
+  }
 
   // this function returns an array of Node values using BFS
   breadthFirstSearch(start) {
-    if (curr === start) return start;
+    const queue = [start];
+    const result = [];
+    const visited = new Set();
+    let currentVertex;
 
-    let toVisit = [start];
+    // visit node
+    visited.add(start);
 
-    while (toVisit.length) {
-      let curr = toVisit.shift();
+    while (queue.length) {
+      currentVertex = queue.shift();
+      result.push(currentVertex.value);
+
+      currentVertex.adjacent.forEach((neighbor) => {
+        if (!visited.has(neighbor)) {
+          visited.add(neighbor);
+          queue.push(neighbor);
+        }
+      });
     }
+    return result;
   }
 }
-
-module.exports = {Node, Graph}
+module.exports = { Node, Graph };
